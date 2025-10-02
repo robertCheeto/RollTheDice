1e81af5c525bd4409081093ce7d3881378beb949
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 11:21:23 -0400

    * fixed looping issue by removing rouge i += 1; * removed working comments

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,10 +2,7 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        // roll the dice 100 times
-        // display the value of dice each time they are rolled
-        // count how many times 2,4,6, and 7 are rolled and display the result
-        //int roll1 = dice.roll(), roll2 = dice.roll();
+
         Dice roll1 = new Dice();
         Dice roll2 = new Dice();
         int twoCounter = 0, fourCounter = 0, sixCounter = 0, sevenCounter = 0;
@@ -15,7 +12,6 @@ public class Main {
             int dice2 = roll2.roll();
             int rollSum = dice1 + dice2;
             System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i, dice1, dice2, rollSum);
-            i += 1;
             switch(rollSum) {
                 case 2:
                     twoCounter += 1;
@@ -32,6 +28,5 @@ public class Main {
             }
         } // end of for loop
         System.out.printf("\n*** Special Roll Counter ***\n2 Counter: %d\n4 Counter: %d\n6 Counter: %d\n7 Counter: %d", twoCounter, fourCounter, sixCounter, sevenCounter);
-
-    }
+    } // end of main
 }
