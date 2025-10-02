ca9e279403adc0443e71fc56965567a72ec3de92
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 11:28:52 -0400

    * removed getter/setter because they are not needed * removed static argument from roll()

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -7,15 +7,7 @@ public class Dice {
         this.dice1 = 0;
     }
 
-    public int getDice1() {
-        return dice1;
-    }
-
-    public void setDice1(int dice1) {
-        this.dice1 = dice1;
-    }
-
-    public static int roll(){
+    public int roll(){
         int maxValue = 6;
         int minValue = 1;
         int randomNumber = (int)(Math.random() * maxValue) + minValue;
