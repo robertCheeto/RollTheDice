3e636e7f30464197cd2533e8648029cb930e79d6
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 11:39:27 -0400

    * formatting changes

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -7,11 +7,13 @@ public class Main {
         Dice roll2 = new Dice();
         int twoCounter = 0, fourCounter = 0, sixCounter = 0, sevenCounter = 0;
 
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             int dice1 = roll1.roll();
             int dice2 = roll2.roll();
             int rollSum = dice1 + dice2;
+
             System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i + 1, dice1, dice2, rollSum);
+
             switch(rollSum) {
                 case 2:
                     twoCounter += 1;
@@ -25,8 +27,8 @@ public class Main {
                 case 7:
                     sevenCounter += 1;
                     break;
-            }
+            } // end of switch statement
         } // end of for loop
         System.out.printf("\n*** Special Roll Counter ***\n2 Counter: %d\n4 Counter: %d\n6 Counter: %d\n7 Counter: %d", twoCounter, fourCounter, sixCounter, sevenCounter);
-    } // end of main
+    } // end of main()
 }
