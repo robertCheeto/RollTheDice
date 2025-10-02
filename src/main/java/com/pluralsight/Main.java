4dace7383906f2ce53aa3c4cfb5df1cddb6bd85e
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 10:44:00 -0400

    * created setters to set values of dice1 and dice2 / not sure if they work

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,14 +5,14 @@ public class Main {
         // roll the dice 100 times
         // display the value of dice each time they are rolled
         // count how many times 2,4,6, and 7 are rolled and display the result
-        int dice1 = 0, dice2 = 0;
+        int roll1 = dice.roll(), roll2 = dice.roll();
         //int twoCounter, fourCounter, sixCounter, sevenCounter = 0;
 
         for (int i = 0; i < 100; i++){
-            dice1.roll();
-            dice2.roll();
-            int diceCount = dice1.roll() + dice2.roll();
-            System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i + 1, dice1, dice2, diceCount);
+            roll1.dice.roll();
+            roll2.dice.roll();
+            int rollSum = roll1.dice.roll() + roll2.dice.roll();
+            System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i + 1, roll1, roll2, rollSum);
             i += 1;
         } // end of for loop
     }
