3b3f26dd6a6fd20cde116916f7815462024a1c08
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 10:55:03 -0400

    * still cannot figure out why i'm getting primitive int type

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -2,26 +2,24 @@ package com.pluralsight;
 
 public class Dice {
     private int dice1;
-    private int dice2;
 
     public Dice() {
         this.dice1 = 0;
-        this.dice2 = 0;
     }
 
     public int getDice1() {
-        return dice1;
+        return dice1.roll();
     }
 
-    public int getDice2() {
-        return dice2;
+    public void setDice1(int dice1) {
+        this.dice1 = dice1;
     }
 
-
-    public static void roll(){
+    public static int roll(int diceRoll){
         int maxValue = 6;
         int minValue = 1;
         int randomNumber = (int)(Math.random() * maxValue) + minValue;
         System.out.println(randomNumber);
+        return diceRoll;
     } // end of roll
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,14 +5,16 @@ public class Main {
         // roll the dice 100 times
         // display the value of dice each time they are rolled
         // count how many times 2,4,6, and 7 are rolled and display the result
-        int roll1 = dice.roll(), roll2 = dice.roll();
+        //int roll1 = dice.roll(), roll2 = dice.roll();
+        Dice roll1 = new Dice();
+        Dice roll2 = new Dice();
         //int twoCounter, fourCounter, sixCounter, sevenCounter = 0;
 
         for (int i = 0; i < 100; i++){
-            roll1.dice.roll();
-            roll2.dice.roll();
-            int rollSum = roll1.dice.roll() + roll2.dice.roll();
-            System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i + 1, roll1, roll2, rollSum);
+            int dice1 = roll1.roll();
+            int dice2 = roll2.roll();
+            int rollSum = dice1 + dice2;
+            System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i + 1, dice1, dice2, rollSum);
             i += 1;
         } // end of for loop
     }
