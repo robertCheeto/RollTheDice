0c45fab65fba4ae173c76498bfc37052c8dff39b
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-02 11:19:42 -0400

    * editing for loop to display correct values * start working on counter for dice rolls - complete * TO-DO: counter is only counting half the rolls it should make, fix this issue

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,14 +8,30 @@ public class Main {
         //int roll1 = dice.roll(), roll2 = dice.roll();
         Dice roll1 = new Dice();
         Dice roll2 = new Dice();
-        //int twoCounter, fourCounter, sixCounter, sevenCounter = 0;
+        int twoCounter = 0, fourCounter = 0, sixCounter = 0, sevenCounter = 0;
 
-        for (int i = 0; i < 100; i++){
+        for (int i = 1; i < 101; i++){
             int dice1 = roll1.roll();
             int dice2 = roll2.roll();
             int rollSum = dice1 + dice2;
-            System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i + 1, dice1, dice2, rollSum);
+            System.out.printf("Roll %d: %d and %d | Roll Sum: %d\n", i, dice1, dice2, rollSum);
             i += 1;
+            switch(rollSum) {
+                case 2:
+                    twoCounter += 1;
+                    break;
+                case 4:
+                    fourCounter += 1;
+                    break;
+                case 6:
+                    sixCounter += 1;
+                    break;
+                case 7:
+                    sevenCounter += 1;
+                    break;
+            }
         } // end of for loop
+        System.out.printf("\n*** Special Roll Counter ***\n2 Counter: %d\n4 Counter: %d\n6 Counter: %d\n7 Counter: %d", twoCounter, fourCounter, sixCounter, sevenCounter);
+
     }
 }
